37516917910b7942e88e47b462005e8de5ff77d6
SerigneFallouNiang <fallouniang776@gmail.com>
2024-07-20 08:04:39 +0000

    affichage des produits

--- js/connexion.js
+++ js/connexion.js
@@ -44,3 +44,58 @@ document.querySelector('#produits').addEventListener('submit', async function(e)
     }
 });
 
+
+
+
+
+
+
+
+
+
+
+
+// Fonction pour afficher les produits depuis Supabase
+async function afficherProduits() {
+    try {
+        const { data: produits, error } = await database
+            .from('produits')
+            .select('*');
+        
+        if (error) throw error;
+
+        const produitList = document.querySelector('#produit-list');
+        produitList.innerHTML = ''; // Réinitialiser la liste des produits
+
+        produits.forEach(produit => {
+            const produitElement = document.createElement('div');
+            produitElement.classList.add('card', 'mt-4');
+            produitElement.innerHTML = `
+             <div class="card-body">
+                            <div class="d-flex justify-content-between align-items-center">
+                <div class="produit-left flex d-flex">
+                    <div>
+                        <img src="https://img.freepik.com/photos-gratuite/woman-shopping-legumes-au-supermarche_1157-37876.jpg?ga=GA1.1.1272467380.1720960746&semt=ais_user" alt="" style="width: 80px;">
+                    </div>
+                    <div class="ml-6">
+                        <h4><strong>${produit.produit}</strong></h4>
+                        <p class="mt-2">${produit.prix}f</p>
+                    </div>
+                </div>
+                <div class="date-globale ml-8 justify-content-end">
+                    <p style="font-size: 12px;color: grey;">${produit.date}</p>
+                    <p class="mt-2" style="font-size: 14px;color: grey;">Globale: ${produit.globale}f</p>
+                </div>
+                    </div>
+                </div>
+            `;
+            produitList.appendChild(produitElement);
+        });
+    } catch (error) {
+        console.error('Erreur lors de la récupération des produits:', error);
+    }
+}
+
+// Appel initial pour afficher les produits lorsqu'on charge la page
+afficherProduits();
+
